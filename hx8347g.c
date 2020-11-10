137da23031718e34fb363160235bf80561fc1bac
Ali Labbene <ali.labbene@st.com>
2020-11-10 12:15:48 +0100

    Release v1.1.2

--- hx8347g.c
+++ hx8347g.c
@@ -2,35 +2,17 @@
   ******************************************************************************
   * @file    hx8347g.c
   * @author  MCD Application Team
-  * @version V1.1.0
-  * @date    10-February-2015
   * @brief   This file includes the LCD driver for HX8347G LCD.
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; COPYRIGHT(c) 2015 STMicroelectronics</center></h2>
+  * <h2><center>&copy; Copyright (c) 2016 STMicroelectronics.
+  * All rights reserved.</center></h2>
   *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
   *
   ******************************************************************************
   */ 
@@ -125,8 +107,12 @@ void hx8347g_Init(void)
   if(Is_hx8347g_Initialized == 0)
   {
     Is_hx8347g_Initialized = 1;
+    
     /* Initialise HX8347G low level bus layer --------------------------------*/
     LCD_IO_Init();
+    
+    /* HX8347G requests 120ms (worst case) after reset */
+    LCD_Delay(120);
     
     /* Driving ability setting */
     hx8347g_WriteReg(LCD_REG_234, 0x00);
@@ -266,6 +252,9 @@ uint16_t hx8347g_ReadID(void)
   if(Is_hx8347g_Initialized == 0)
   {
     LCD_IO_Init();
+
+    /* HX8347G requests 120ms (worst case) after reset */
+    LCD_Delay(120);
   }
   return (hx8347g_ReadReg(0x00));
 }
@@ -341,6 +330,9 @@ void hx8347g_WriteReg(uint8_t LCDReg, uint16_t LCDRegValue)
   */
 uint16_t hx8347g_ReadReg(uint8_t LCDReg)
 {
+  /* Write 16-bit Index (then Read Reg) */
+  LCD_IO_WriteReg(LCDReg);
+
   /* Read 16-bit Reg */
   return (LCD_IO_ReadData(LCDReg));
 }
